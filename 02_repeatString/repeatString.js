ea030ae68632a702dc9239e0b8c29b07cd93ecbb
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2023-10-27 16:41:31 -0700

    Completed exercise 2!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+function repeatString(string, num) {
+    let newString = '';
+    if (num < 0) {
+        return "ERROR";
+    }
+    else{
+        for(let loopNum = 0; loopNum < num; loopNum++){
+            newString += string
+            console.log(newString);
+        }
+        return newString
+    }
+ 
 };
+
+repeatString('hey',3);
 
 // Do not edit below this line
 module.exports = repeatString;
