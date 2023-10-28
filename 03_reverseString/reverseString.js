9849911a7fe7d143407112d728efcfe04e747d48
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2023-10-27 17:09:49 -0700

    Completed reverse string exercise.
    1. Split String into array of chars
    2. Reverse array
    3. Join array back into string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
 
+    //may also chain functions like so: return splitString.split("").reverse().join("");
+
+    return joinArray;
 };
+
+console.log(reverseString('Hello'));
 
 // Do not edit below this line
 module.exports = reverseString;
