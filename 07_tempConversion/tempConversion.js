8426f56b61b800b4120d9245cfca4513576c5296
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2023-10-30 16:47:08 -0700

    Create two functions that allow conversion of temperature between: 1. Celcius to Fahrenheit 2. Fahrenheit to Celcius
    These round the result to 1 decimal place precision and functions
    properly with negative numbers

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  celcius = (temp -32) * (5/9);
+  celcius = parseFloat(celcius.toFixed(1));
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  fahrenheit = temp * (9/5) + 32;
+  fahrenheit = parseFloat(fahrenheit.toFixed(1));
+  return fahrenheit;
 };
 
 // Do not edit below this line
