3f116aad747f4e177697e26788413da24d692148
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2023-10-29 19:43:01 -0700

    completed exercise 05 1. created function that inputs two numbers and sums all numbers between those two inputs. 2. If any negative numbers or non-integer inputs are given it will throw an error. 3. Can input larger number first or smaller, both will work.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,38 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sumTotal = 0;
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        console.log('ERROR');
+        return('ERROR');
+    }
+      
+    else if (start < 0 || end < 0) {
+        console.log('ERROR');
+        return('ERROR');
+    }
+    else if (start < end && start > 0 && end > 0) {
+        for(let sumStep = start; sumStep <= end; sumStep++) {
+            console.log('sumStep ' + sumStep);
+            sumTotal += sumStep;
+            console.log('sumTotalloop ' + sumTotal);
+        }
+    }
+  
+    else if (start > end && start > 0 && end > 0){ 
+        console.log('Start is larger than end');
+        for(let sumStep = start; sumStep >= end; sumStep--) {
+            console.log('sumStep ' + sumStep);
+            sumTotal += sumStep;
+            console.log('sumTotalloop ' + sumTotal);
+        }
+    }
+    else{ 
+        console.log('ERROR');
+        return('ERROR');
+    }
+    console.log('Final Total:' + sumTotal);
+    return sumTotal;
 };
+sumAll(10,"90");
 
 // Do not edit below this line
 module.exports = sumAll;
