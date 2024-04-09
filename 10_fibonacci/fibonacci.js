337df988b97d40ed63ca71f44fa0dd3e9eb80c48
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2024-04-09 16:17:15 -0700

    Completed all exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,29 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    convertedNum = Number(num);
+    if (convertedNum === 0) {
+        return 0;
+    }
+    else if (convertedNum === 1 || convertedNum === 2) {
+        return 1;
+    }
+    else if (convertedNum > 2) {
+        let fibArray = [0, 1, 1];
+        for (let iteration = 3; iteration <= convertedNum; iteration++) {
+            let fibValue = fibArray[iteration - 1] + fibArray[iteration - 2];
+            fibArray.push(fibValue);
+        };
+        let fibElement = fibArray.pop();
+        return fibElement;
+    }
+    else {
+        return 'OOPS';
+    };
 };
+
+// ```javascript
+// fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
+// fibonacci(6); // returns 8
+// ```
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,99 @@
-const findTheOldest = function() {
+const findTheOldest = function (obj) {
+    oldestSort = obj.sort(function (a, b) {
 
+        if (a.hasOwnProperty(('yearOfDeath')) && b.hasOwnProperty('yearOfDeath')) {
+            let lastGuy = a.yearOfDeath - a.yearOfBirth;
+            let nextGuy = b.yearOfDeath - b.yearOfBirth;
+            return lastGuy > nextGuy ? -1 : 1;
+        }
+
+        else {
+            let currentYear = new Date().getFullYear()  // returns the current year
+            let lastGuy = currentYear - a.yearOfBirth;
+            let nextGuy = currentYear - b.yearOfBirth;
+            return lastGuy > nextGuy ? -1 : 1;
+        }
+    });
+    oldPerson = oldestSort[0];
+    //console.log(oldPerson);
+    return oldPerson;
 };
+//# Exercise 12 - Find the Oldest
+
+// Given an array of objects representing people with a birth and death year, return the oldest person.
+
+// Now that you've reached the final exercise, you should be fairly comfortable getting the information you need from test case(s). Take a look at how the array of objects is constructed in this exercise's test cases to help you write your function.
+
+// ## Hints
+//     - You should return the whole person object, but the tests mostly just check to make sure the name is correct.
+// - This can be done with a couple of chained array methods, or by using`reduce`.
+// - One of the tests checks for people with no death - date..use JavaScript's Date function to get their age as of today.
+
+
+
+//const findTheOldest = require('./findTheOldest')
+// describe('findTheOldest', () => {
+//   test('finds the person with the greatest age!', () => {
+//     const people = [
+//       {
+//         name: "Carly",
+//         yearOfBirth: 1942,
+//         yearOfDeath: 1970,
+//       },
+//       {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
+//       },
+//       {
+//         name: "Jane",
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941,
+//       },
+//     ]
+//     expect(findTheOldest(people).name).toBe('Ray');
+//   });
+//   test.skip('finds the person with the greatest age if someone is still living', () => {
+//     const people = [
+//       {
+//         name: "Carly",
+//         yearOfBirth: 2018,
+//       },
+//       {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
+//       },
+//       {
+//         name: "Jane",
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941,
+//       },
+//     ]
+//     expect(findTheOldest(people).name).toBe('Ray');
+//   });
+//   test.skip('finds the person with the greatest age if the OLDEST is still living', () => {
+//     const people = [
+//       {
+//         name: "Carly",
+//         yearOfBirth: 1066,
+//       },
+//       {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
+//       },
+//       {
+//         name: "Jane",
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941,
+//       },
+//     ]
+//     expect(findTheOldest(people).name).toBe('Carly');
+//   });
+// });
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
